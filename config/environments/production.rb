e77f503a5e248744f8d29a339aa30ebb41c3b2cb
David Yao <davidyao@Davids-MacBook-Air.local>
2023-07-10 19:57:51 -0500

    add david-yao.up.railway.app to prod and dev en

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts = [
-    "david-yao.up.railway.app"
-  ]
+  config.hosts << "david-yao.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
